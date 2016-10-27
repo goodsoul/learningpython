9caada541eaad9f23cce47e99997b0834b6785ff
goodsoul <black-billye@163.com>
2016-10-27 16:51:09 +0800

    Add files via upload

---
+++ 04_datatype.py
@@ -0,0 +1,17 @@
+#print practice
+n=123
+f=456.789
+s1='hello, world'
+s2='hello, \n\'Adam\''
+s3=r'hello, "Bart"'
+s4=r'''Hello,
+Lisa!'''
+
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+
