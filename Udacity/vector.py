a2f614e37569d7dbe75e65e6a5c5cd3b889e53e8
goodsoul <black-billye@163.com>
2017-11-02 22:34:42 +0800

    Linear Algebra

---
+++ Udacity/vector.py
@@ -0,0 +1,164 @@
+import math
+import decimal
+
+decimal.getcontext().prec = 30
+
+class Vector(object):
+
+    """Constant message"""
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def plus(self, v):
+        """Perform plus action for two vectors"""
+        new_coordinates = [x+y for x,y in zip(self.coordinates,v.coordinates)]
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        """Perform minus action for two vectors"""
+        new_coordinates = [x-y for x,y in zip(self.coordinates,v.coordinates)]
+        return Vector(new_coordinates)
+
+    def time_scalar(self, c):
+        """Perform time scalar action for vector"""
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude(self):
+        """
+            Calculate length of unit vector:
+            Keyword arguments:
+                self -- vector
+        """
+        s=0
+        for x in self.coordinates:
+            s+=x*x
+        return math.sqrt(s)
+
+    def normalized(self):
+        """ Transfer vector to normal vector:
+            Keyword arguments:
+                self -- vector
+        """
+        try:
+            magnitude=self.magnitude()
+            return self.time_scalar(1./magnitude)
+
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
+    def dot_product(self,v):
+        """ Perform dot product for two vectors:
+            Keyword arguments:
+                self -- vector
+                v -- 2nd vector
+            Math Formula:
+                v*m=v1*w1 + v2*w2 + ... + vn*wn
+        """
+        new_coordinates = [x*y for x,y in zip(self.coordinates,v.coordinates)]
+        return sum(new_coordinates)
+
+    def angle(self, v,output_type):
+        """ Calculate the angle :
+            Keyword arguments:
+                self -- vector
+                v -- 2nd vector
+                output_type -- 'd'-degree / 'r'-rad
+        """
+        try:
+            angle = math.acos(self.normalized().dot_product(v.normalized()))
+        
+            if output_type=='d':
+                degrees_per_radians = 180./math.pi
+                return angle*degrees_per_radians
+            if output_type=='r':
+                return angle
+            #else:
+            #    return 'Please input correct output type;'
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise e
+ 
+    def is_zero(self,tolerance=1e-10):
+        return self.magnitude()<tolerance        
+
+    def check_para_orth(self,v,tolerance=1e-10):
+        """ Check for Parallelism & Orthogonality:
+            Keyword arguments:
+                self -- vector
+                v -- 2nd vector
+            Output:
+                Result for Parallel & Orthogonality
+            
+        """ 
+        isorth = False 
+        ispara = False
+
+        if (self.is_zero() or v.is_zero()):
+            ispara = True
+            isorth = True 
+
+        else:
+            if 1-abs(self.normalized().dot_product(v.normalized()))<tolerance:
+                ispara = True
+
+            if abs(self.normalized().dot_product(v.normalized()))<tolerance:
+                isorth = True
+
+        return 'Paralell:'+str(ispara)+' ; Orthogonal:'+str(isorth)
+
+    def component_parallel_to(self,b):
+        """Calculate the project vector:
+            Key arguments:
+                self -- vector
+                b -- baseline vector
+            Output:
+                Project vector on baseline vector
+            Formula:
+                project vector = (v*ub)*ub
+            
+        """
+        if b.is_zero()==False:
+            b_n=b.normalized()
+            c=self.dot_product(b_n)
+            return b_n.time_scalar(c)
+        return b
+
+    def component_orthogonal_to(self,b):
+        """Calculate the project vector:
+            Key arguments:
+                self -- vector
+                b -- baseline vector
+            Output:
+                Orthogonal vector For baseline project
+            Formula:
+                vector v = v's project vector + b's orthogonal vector
+                b's orthogonal vector = vector v - v's project vector
+        """        
+        return self.minus(self.component_parallel_to(b))
+
+    def component_cross_product(self,v):
+        return
---
+++ Udacity/vector_test.py
@@ -0,0 +1,68 @@
+#import test.vector as vector
+import vector
+import math
+
+v1 = vector.Vector([7.887,4.138])
+w1 = vector.Vector([-8.802,6.776])
+
+v2 = vector.Vector([-5.955,-4.904,-1.874])
+w2 = vector.Vector([-4.496,-8.755,7.103])
+
+v3 = vector.Vector([3.183,-7.627])
+w3 = vector.Vector([-2.668,5.319])
+
+v4 = vector.Vector([7.35,0.221,5.188])
+w4 = vector.Vector([2.751,8.259,3.985])
+
+#--------------------------------------
+
+v5 = vector.Vector([-7.579,-7.88])
+w5 = vector.Vector([22.737,23.64])
+
+v6 = vector.Vector([-2.029,9.97,4.172])
+w6 = vector.Vector([-9.231,-6.639,-7.245])
+
+v7 = vector.Vector([-2.328,-7.284,-1.214])
+w7 = vector.Vector([-1.821,1.072,-2.94])
+
+v8 = vector.Vector([2.118,4.827])
+w8 = vector.Vector([0,0])
+
+#--------------------------------------
+v9 = vector.Vector([3.039,1.879])
+b9 = vector.Vector([0.825,2.036])
+
+v10 = vector.Vector([-9.88,-3.264,-8.159])
+b10 = vector.Vector([-2.155,-9.353,-9.473])
+
+v11 = vector.Vector([3.009,-6.172,3.692,-2.51])
+b11 = vector.Vector([6.404,-9.144,2.759,8.718])
+
+#print v1.magnitude()
+#print v2.magnitude()
+#print v3.normalized()
+#print v4.normalized()
+
+'''
+print v1.dot_product(w1)
+print v2.dot_product(w2)
+print v3.angle(w3,'r')
+print v4.angle(w4,'d')
+'''
+
+print v5.check_para_orth(w5)
+print v6.check_para_orth(w6)
+print v7.check_para_orth(w7)
+print v8.check_para_orth(w8)
+
+print v9.component_parallel_to(b9)
+print v10.component_orthogonal_to(b10)
+
+print v11.component_parallel_to(b11)
+print v11.component_orthogonal_to(b11)
+
+#print v2==v3
+#print v1==v3
+#print v4
+#print v5
+#print v6
